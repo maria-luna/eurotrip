404335b5835bf8248dacf89e0456c6c5d7e3a58a
maria-luna <46219818+maria-luna@users.noreply.github.com>
2018-12-28 15:26:36 +0100

    Add files via upload

---
+++ js/botones.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+
+function popup2() {
+  var popup = document.getElementById("myPopup2");
+  popup.classList.toggle("show");
+}
